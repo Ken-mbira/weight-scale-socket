93b409a53c7d67391855d8a0a00c4a7af606ae21
Ken-mbira <devmbira@gmail.com>
2022-07-27 12:54:04 +0300

    set up complete websocket system

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ import websockets
 async def echo(websocket):
     print("I have been initiated")
     async for message in websocket:
-        print(f"I have been called and the message that i got is {message}")
         await websocket.send(message)
 
 async def main():
