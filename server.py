d9014729a7c311994116e4bb0c85c04681c23e00
Ken-mbira <devmbira@gmail.com>
2022-07-27 11:19:16 +0300

    create a connection to websockets

--- server.py
+++ server.py
@@ -3,9 +3,11 @@
 import asyncio
 import websockets
 
-async def echo(websockets):
-    async for message in websockets:
-        await websockets.send(message)
+async def echo(websocket):
+    print("I have been initiated")
+    async for message in websocket:
+        print(f"I have been called and the message that i got is {message}")
+        await websocket.send(message)
 
 async def main():
     async with websockets.serve(echo,"localhost",8765):
